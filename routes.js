49e7cdd2a839495d2b60179ef3b37df0f558b4d4
João André <88809816+Jottoss@users.noreply.github.com>
2021-08-18 07:47:25 +0000

    Att 18/08

--- routes.js
+++ routes.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const router = express.Router();
-//const faker = require("faker");
 
-//let db = require("./db");
+// const faker = require("faker");
 
+// let db = require("./db");
+
+// db.criarDB("minhaBaseDados");
+
+//Especifica a pasta contendo arquivos estáticos. 
+//O nome 'public' não precisará ser colocado na rota 
+//Para serem alcançados os arquivos e pastas que estão dentro dele. 
+//Por isso na imagem que está na página home.ejs só há o indicativo para 'images'
 router.use(express.static('public'));
 
-router.get('/', (req, res) => {
-  res.render('pages/home');
-  res.send('Página de teste');
-  //fazer as outras routes
+
+
+//************* Exemplode Rotas ************* 
+
+/*
+http://localhost:3030/css
+http://localhost:3030/images
+http://localhost:3030/index.html
+/ = http://localhost:3030/
+/about = http://localhost:3030/about
+/cadastro =  http://localhost:3030/cadastro
+*/
+
+
+//Req é um objeto que recebe dados da requisição HTTP feita (request). Res permite enviar uma resposta ao navegador (Response)
+router.get('/',(req,res)=>{ //callback - funcao que trata dado evento GET
+    res.render('pages/home');
 });
 
-router.get('/about', (req, res) => {
-  res.send('Minha página Sobre');
+router.get('/about',(req,res)=>{ //callback - funcao que trata dado evento  GET
+
+    res.render('pages/about');
 });
 
 router.get('/cadastro',(req,res)=>{ //callback - funcao que trata dado evento  GET
@@ -24,13 +45,46 @@ router.get('/cadastro',(req,res)=>{ //callback - funcao que trata dado evento  G
 });
 
 router.post('/cadastro/remove',(req,res)=>{
-    let item =req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
+    //let item =req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
+    let name = req.body.name;
 
-    users.splice(item,1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
+    if(users.length==0){
+        console.log("Erro: Não há elemento a ser removido!");
+        return res.status(400).json({
+            status:'error',
+            error:`Removed element: ${name}`
+        });
+
+    } else {
+        for(let cont=0;cont<users.length;cont++){
+            if(users[cont].name==name){
+                users.splice(cont,1);
+                console.log("Elemento Removido: ",name);
+                return res.status(200).json({
+                    status:'sucess',
+                    data:users
+                });
+                //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+            } else if(cont==users.length-1){
+                console.log("Erro ao remover elemento: ",name);
+                return res.status(400).json({
+                    status:'error',
+                    error:`Removed element: ${name}`
+                });
+            }
+        }
+    }
+    
+    
+    //users.splice(item,1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
     //res.render('pages/cadastro',{users:users});
-    console.log("Elementos cadastrados: ",users);
-    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
+    //res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
+    //res.send(JSON.stringify({sucess:`Elemento removido com sucesso: ${name}`}));
+    //console.log("Elemento Removido: ",name);
+    
 });
+
+
 
 router.post('/cadastro/update',(req,res)=>{
     //substitui os valores armazenados no item do vetror dado por id, por valores fornecidos como parametro vindos do navegador.
@@ -49,6 +103,12 @@ router.post('/cadastro/update',(req,res)=>{
 });
 
 router.get('/cadastro/list',(req,res)=>{
+    //Para fazer em casa: Como seria uma rotina para listar todos os itens cadastrados?
+
+});
+
+router.post('/cadastro/addUser',(req,res)=>{
+    //Para fazer em casa: Como seria uma rotina para listar todos os itens cadastrados?
 
 });
 
