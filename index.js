05fee2be6b4d752fb06e15e794b225f0a8118088
João André <88809816+Jottoss@users.noreply.github.com>
2021-08-24 10:16:50 +0000

    Atualização 24/08!

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { urlencoded } = require("express")
 require("ejs");
 require("express-ejs-layouts");
 
+const address = "localhost";
+const utils = require("./utils");
+const faker = require("faker");
+let toggleBol=true;
 
 app.set('view engine','ejs');
 
@@ -14,10 +18,12 @@ const port = process.env.PORT || 3000;
 //const address = "localhost"; //localhost é o nome padrao do seu computador e está ligado ao IP 127.0.0.1
 
 // a palavra reservada global cria uma variável ou objeto global para o sistemas. Ele pode ser visto em qualquer parta do código ou dos módulos do projeto. Assim, Users podem ser vistos tanto aqui no index.js quanto em routes.js
-global.users = [
-  { name: "Wellington W. F. Sarmento", address: "Rua Dom Jeronimo, 666", email: "wwagner@virtual.ufc.br", age: 46, height: 1.70, vote: true },
-  { name: "PAtricia S. Paula", address: "Rua Dom Jeronimo, 666", email: "patricia@virtual.ufc.br", age: 46, height: 1.70, vote: true },
-  { name: "Henrique Sérgio L. Pequeno", address: "Rua do Henrique, 666", email: "henrique@virtual.ufc.br", age: 46, height: 1.70, vote: true }];
+global.users =[];
+
+for (let cont=0;cont<30;cont++){
+    users.push({name:faker.name.findName(),email:faker.internet.email(),address:faker.address.streetAddress(),age:utils.getRandomByInterval(15,50,true),heigth:utils.getRandomByInterval(1.50,1.70,false).toFixed(2),vote:toggleBol});
+    toggleBol=!toggleBol;
+}
 
 //ativa uso do EJS e do Express-ejs-layouts
 app.set('view engine', 'ejs');
